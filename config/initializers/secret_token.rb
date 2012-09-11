60f124e0f0bb78128caf5f4a407ba45506cc72e9
Denis Jacquemin <denis.jacquemin@gmail.com>
2012-09-11 19:23:23 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Holiday::Application.config.secret_token = '881d6062cceb33bbb75a85f8b431f2e7ae367d8408bab514dc307a5dd8e92223dfdbea66908538c48576355287e94071cb4a3cd063a58fb758117321a1200613'
