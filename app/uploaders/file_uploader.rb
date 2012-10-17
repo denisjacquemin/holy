0aacffa954473b508d213e0b5590a003c748c641
Denis Jacquemin <denis.jacquemin@gmail.com>
2012-10-17 19:04:41 +0200

    styling images on item details page

--- app/uploaders/file_uploader.rb
+++ app/uploaders/file_uploader.rb
@@ -40,6 +40,10 @@ class FileUploader < CarrierWave::Uploader::Base
      process :resize_to_limit => [200, 200]
   end
 
+  version :normal do
+     process :resize_to_limit => [450, 450]
+  end
+
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   # def extension_white_list
