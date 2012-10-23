324557f731afab20709ff71718a984b11bdb9b5f
Denis Jacquemin <denis.jacquemin@gmail.com>
2012-10-23 07:44:10 +0200

    default scope for images is order(:position)

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,10 +1,14 @@
 class Image < ActiveRecord::Base
+  acts_as_list
+  
   attr_accessible :file, :name, :item_id
   mount_uploader :file, FileUploader
   
   belongs_to :item
   
   before_create :default_name
+  
+  default_scope order('position')
   
   def default_name
     self.name ||= File.basename(file.filename, '.*').titleize if file
